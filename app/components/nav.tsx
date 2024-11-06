34f7ba2535f3b5ef0cde6dde38ed72457d69acd0
minseok <imuchgabis@gmail.com>
2024-11-06 12:26:42 +0900

    이메일, 이력서 추가

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -14,37 +14,37 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full mb-14 ">
-      <div className="flex items-center justify-between max-w-6xl mx-auto">
-        {/* Logo */}
-        <Link href="/" className=" flex items-center gap-2">
-          <Image
-            src="/icon.png" // public 폴더 안의 파일을 이렇게 참조
-            alt="My Icon"
-            width={64} // 원하는 너비
-            height={64} // 원하는 높이
-          />
-          <h1 className="text-3xl font-bold">FE민석</h1>
-        </Link>
+    <>
+      {/* 네비게이션 바의 높이만큼 공간을 확보하기 위한 더미 div */}
+      <div className="h-24" /> {/* 필요한 경우 높이 조절 */}
+      {/* Fixed 네비게이션 바 */}
+      <div className="fixed top-0 left-0 right-0 w-full bg-white dark:bg-black z-50 ">
+        <div className="flex items-center justify-between max-w-2xl mx-auto h-24">
+          {/* Logo */}
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/icon.png" alt="My Icon" width={64} height={64} />
+            <h1 className="text-3xl font-bold">FE민석</h1>
+          </Link>
 
-        {/* Navigation Links */}
-        <nav className="flex items-center space-x-6">
-          {/* Theme Toggle Button */}
-          <ThemeSwitcher />
+          {/* Navigation Links */}
+          <nav className="flex items-center space-x-6">
+            {/* Theme Toggle Button */}
+            <ThemeSwitcher />
 
-          {/* Navigation Items */}
-          {Object.entries(navItems).map(([path, { name }]) => (
-            <Link
-              key={path}
-              href={path}
-              className=" text-xl transition-all duration-200 hover:text-neutral-800 dark:hover:text-neutral-200 text-neutral-600 dark:text-neutral-400"
-            >
-              {name}
-            </Link>
-          ))}
-        </nav>
+            {/* Navigation Items */}
+            {Object.entries(navItems).map(([path, { name }]) => (
+              <Link
+                key={path}
+                href={path}
+                className="text-xl transition-all duration-200 hover:text-neutral-800 dark:hover:text-neutral-200 text-neutral-600 dark:text-neutral-400"
+              >
+                {name}
+              </Link>
+            ))}
+          </nav>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
